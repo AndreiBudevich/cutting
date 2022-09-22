ec75383e6c2ef3895a0140602e424b0148b4c004
budevichac01@gmail.com <budevichac01@gmail.com>
2022-09-22 14:40:20 +0300

    create BaseEntity

---
+++ src/main/java/com/upmebel/cutting/CuttingApplication.java
@@ -0,0 +1,11 @@
+package com.upmebel.cutting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuttingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CuttingApplication.class, args);
+	}
+}
---
+++ src/main/java/com/upmebel/cutting/model/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.upmebel.cutting.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.upmebel.cutting.HasId;
+import lombok.*;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseEntity implements HasId {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    @JsonIgnore
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !this.getClass().equals(o.getClass())) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " id:" + id;
+    }
+}
